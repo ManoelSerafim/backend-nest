2fff008295709db2f1e498dafb1fc3e4257db369
Manoel Serafim <manoeldserafim@gmail.com>
2026-05-29 17:57:34 +0000

    teste

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TarefasModule } from './tarefas/tarefas.module';
+import { TesteController } from './teste/teste.controller';
 
 @Module({
   imports: [TarefasModule],
-  controllers: [AppController],
+  controllers: [AppController, TesteController],
   providers: [AppService],
 })
 export class AppModule {}
